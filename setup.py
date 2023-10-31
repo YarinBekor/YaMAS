5ffbe6fb1a0fc2f4900d3715824a137abaf6c5c1
YarinBekor <yarin.bekor@gmail.com>
2023-10-31 12:27:09 +0200

    fixed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="YMS",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="1.0.5",
+    version="1.0.6",
     author="Yarin Bekor",
     author_email="yarin.bekor@gmail.com",
     description="YOLO Microbiome Analysis System",
