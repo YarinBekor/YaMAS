b765febb7f74c6c697b47856e3b1e4d248e7de83
shanif3 <shaninn123@gmail.com>
2023-11-07 09:47:32 +0200

    Revert "fixed yamas ready"
    This reverts commit ae0838adf41ffbbedb7791d797d9167cf595eafc.

--- yamas/__init__.py
+++ yamas/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 from .dataset_downloading import download
 from .export_data import export
 from .prerun_configs import set_enviorment
- 
+
 def main():
     # Initialize the argument parser with a description.
     parser = argparse.ArgumentParser(description='YMS package')
@@ -13,13 +13,13 @@ def main():
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s {version}'.format(version=pkg_resources.require("YMS")[0].version))
 
-    parser.add_argument('--ready',nargs=1,choices=['Ubuntu', 'CentOS', 'MacOS'], help='Type of Operating system')
-
+    parser.add_argument('--ready',nargs=1,choices=['16S', 'Shotgun'], help='Type of Operating system')
+    
     # Add an argument for specifying datasets to be downloaded.
     parser.add_argument('--download', nargs='+', help='Add datasets to be downloaded')
 
     # Add an argument for specifying the type of data to be downloaded (16S or Shotgun).
-    parser.add_argument('--type', nargs=1, choices=['16S', '18S', 'Shotgun'], help='Type of data to be downloaded')
+    parser.add_argument('--type', nargs=1, choices=['16S', 'Shotgun'], help='Type of data to be downloaded')
 
     # Add an argument for specifying export parameters.
     parser.add_argument('--export', nargs=6,
@@ -54,14 +54,13 @@ def main():
         try:
             # Extract export parameters from the command line arguments.
             origin_dir = args.export[0]
-            data_type= args.export[1]
-            trim = int(args.export[2])
-            trunc = int(args.export[3])
-            classifier_file = args.export[4]
-            threads = args.export[5]
+            trim = int(args.export[1])
+            trunc = int(args.export[2])
+            classifier_file = args.export[3]
+            threads = args.export[4]
 
             # Call the export function with the specified parameters.
-            export(origin_dir, data_type,trim, trunc, classifier_file, threads)
+            export(origin_dir, trim, trunc, classifier_file, threads)
         except IndexError:
             # Handle the case where the number of export arguments is insufficient.
             print(f"missing {len(args.export)-1} arguments")
--- yamas/prerun_configs.py
+++ yamas/prerun_configs.py
@@ -2,19 +2,16 @@ from .utilities import run_cmd
 
 command_based_on_os = {'Ubuntu': ['wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-ubuntu64.tar.gz'],
                        'CentOS': ['wget --output-document sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-centos_linux64.tar.gz'],
+                       'MacOS': ['curl --output sratoolkit.tar.gz https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/current/sratoolkit.current-mac64.tar.gz']
                        }
 
 
 def set_enviorment(operating_system_type):
     command = command_based_on_os[operating_system_type]
     run_cmd(command)
-    with open('sra_toolkit_download_log.txt', 'w') as file:
-        pass
-    run_cmd(['tar -vxzf sratoolkit.tar.gz > sra_toolkit_download_log.txt 2>&1'])
+    run_cmd(['tar -vxzf sratoolkit.tar.gz > sra_toolkit_download_log.txt'])
     with open('sra_toolkit_download_log.txt', 'r') as file:
-        last_line = None
-        for line in file:
-            last_line = line
+        last_line = file.readlines()[-1]
         version = last_line.split("/")[0]
         run_cmd([f'export PATH=$PATH:$PWD/{version}/bin'])
     
