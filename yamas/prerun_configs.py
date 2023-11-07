ffd95bff12a08e0f5c1167661b55880af4c92c7f
shanif3 <shaninn123@gmail.com>
2023-11-07 14:04:46 +0200

    fixed yamas ready+ update version number

--- yamas/prerun_configs.py
+++ yamas/prerun_configs.py
@@ -6,22 +6,24 @@ command_based_on_os = {'Ubuntu': ['wget --output-document sratoolkit.tar.gz http
 
 
 def set_environment(operating_system_type):
+
+    #defualt. it will update soon based on your os
+    version="sratoolkit.3.0.7-centos_linux64"
+
     command = command_based_on_os[operating_system_type]
-    print("######   Downloading SRA Toolkit   ######")
+    print("######   Downloading SRA Toolkit   ")
     run_cmd(command)
 
-    print("######   Extracting the contents of the tar file   ######")
+    print("######   Extracting the contents of the tar file   ")
     run_cmd(['tar -vxzf sratoolkit.tar.gz > sra_toolkit_download_log.txt'])
     with open('sra_toolkit_download_log.txt', 'r') as file:
         last_line = file.readlines()[-1]
         version = last_line.split("/")[0]
-        print(f"######   export PATH   ######")
-        try:
-            run_cmd([f'export PATH=$PATH:$PWD/{version}/bin'])
-        except Exception as e:
-            print("######   Can't export the path. Failed. See below.   ######")
 
-    print("######   Checking if we are ready to go...   ######")
+    print(f"######   export PATH   ")
+    run_cmd([f'export PATH=$PATH:$PWD/{version}/bin'])
+
+    print("######   Checking if we are ready to go...   ")
     run_cmd(['which fastq-dump > check_fastq-dump.txt'])
     with open('check_fastq-dump.txt', 'r') as check_file:
         content= check_file.read()
