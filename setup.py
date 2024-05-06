1969cd309f5d9e2340a27a63c53773f78b3ff09b
shanif3 <shaninn123@gmail.com>
2024-05-06 13:41:19 +0300

    upgrade version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="YMS",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version="1.2.9",
+    version="1.2.10",
     author="Yarin Bekor, Shani Finkelstein",
     author_email="yarin.bekor@gmail.com, shaninn123@gmail.com",
     description="YOLO Microbiome Analysis System",
